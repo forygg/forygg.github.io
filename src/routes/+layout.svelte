43269033149fb63733c139b3e6ecdfb82d47ce41
forygg <e.spiridonov@innopolis.university>
2023-07-13 04:03:46 +0300

    Fixes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<main>
+    <slot>
+        <header>
+            <nav>
+              <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#comic-generation">Comic</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </nav>
+          </header>
+          <footer>
+            <p>&copy; 2023 All rights reserved.</p>
+          </footer>
+    </slot>
+</main>
